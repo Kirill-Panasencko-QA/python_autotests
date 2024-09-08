8b8eedea10b0e6f3abc931164d51035c42fb2a75
Kirill-Panasencko-QA <kirillgorohovscky@yandex.ru>
2024-09-08 21:27:12 +0300

    Add files via upload

---
+++ Python/PythonProjects/main.py
@@ -0,0 +1,10 @@
+import requests
+
+URL = 'https://api.pokemonbattle.ru/v2'
+TOKEN = '0d73c99635737f97e8c1f2ade9503c5e'
+HEADR = {'Content-Type' : 'application/json'}
+body_create = {
+    "name": "Бульбазавр",
+    "photo_id": -1
+}
+body_change = 
